2646e94ac08e12bc1aefec3c9aabffb2e7e13e5d
Micah Gomez <micahplacido@gmail.com>
2019-05-26 14:01:00 -0600

    initial commit

---
+++ XenimatedRecharged/var/mobile/Library/LockHTML/XenimatedCharge/main.js
@@ -0,0 +1,15 @@
+var wallpaper = "url('../../Xenimated/"+wallgif+"')";
+
+
+
+function mainUpdate(type){
+    if(type == "battery"){
+		var chargeText = (batteryCharging);
+		if (chargeText){
+			document.body.style.backgroundImage = wallpaper;
+		}
+		else{
+			document.body.style.backgroundImage = "none";
+		}
+    }
+}
